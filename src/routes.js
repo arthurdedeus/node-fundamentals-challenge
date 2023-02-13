c8085ff1f02f01b7adb6f9def3fcabe72291ed3d
Arthur Moreira de Deus <arthurdedeus@gmail.com>
2023-02-13 16:26:47 -0300

    project setup

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+export const routes = [
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    // TODO: Implement handler
+  },
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    // TODO: Implement handler
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    // TODO: Implement handler
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    // TODO: Implement handler
+  },
+  {
+    method: 'PATCH',
+    path: buildRoutePath('/tasks/:id/complete'),
+    // TODO: Implement handler
+  },
+]
